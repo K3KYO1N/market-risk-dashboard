ec929f171d7f943413ee7df3836b15f7eebc1fb5
Eric <eric2004y@gmail.com>
2025-05-08 17:54:06 -0400

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -2,9 +2,9 @@ import plotly.graph_objs as go
 
 def plot_returns(returns):
   returns_percent = returns * 100  # Convert to percentage
-    fig = go.Figure([go.Scatter(y=returns_percent, name="Daily Returns")])
-    fig.update_layout(title="Daily Returns", xaxis_title="Date", yaxis_title="Return in %")
-    fig.show()
+  fig = go.Figure([go.Scatter(y=returns_percent, name="Daily Returns")])
+  fig.update_layout(title="Daily Returns", xaxis_title="Date", yaxis_title="Return in %")
+  fig.show()
   # fig = go.Figure([go.Scatter(y=returns, name="Daily Returns")])
   # fig.update_layout(title="Daily Returns", xaxis_title="Date", yaxis_title="Return in %")
   # fig.show()
