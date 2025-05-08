1528d79923845f543df3365b9665d92b3a15ea40
Eric <eric2004y@gmail.com>
2025-05-08 17:39:37 -0400

    Update data_loader.py

--- src/data_loader.py
+++ src/data_loader.py
@@ -3,11 +3,11 @@ import yfinance as yf
 import pandas as pd
 
 def fetch_data(ticker, start, end):
-  ticker_obj = yf.Ticker(ticker)
-  df = ticker_obj.history(start=start, end=end)
-  print(df.columns)
-  df.to_csv(f"data/{ticker}.csv")
-  return df
-  # df = yf.download(ticker, start=start, end=end)
+  # ticker_obj = yf.Ticker(ticker)
+  # df = ticker_obj.history(start=start, end=end)
+  # print(df.columns)
   # df.to_csv(f"data/{ticker}.csv")
   # return df
+  df = yf.download(ticker, start=start, end=end)
+  df.to_csv(f"data/{ticker}.csv")
+  return df
