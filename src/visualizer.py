6c02aba50f5a3b0c89eaab63c6a6cebb2d62f561
Eric <eric2004y@gmail.com>
2025-05-08 16:58:53 -0400

    Update visualizer.py

--- src/visualizer.py
+++ src/visualizer.py
@@ -1,1 +1,7 @@
+import plotly.graph_objs as go
 
+def plot_returns(returns):
+  fig = go.Figure([go.Scatter(y=returns, name="Daily Returns")])
+  fig.update_layoud(title="Daily Returns', xaxis_title="Date", yaxis_title="Return")
+  fig.show()
+  #Creates an interactive line chart using Plotly.
