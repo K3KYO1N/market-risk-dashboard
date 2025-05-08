9ab4c5100f1ef010d89be901e919fa05545a193a
Eric <eric2004y@gmail.com>
2025-05-08 17:45:38 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@ from visualizer import plot_returns
 if __name__ == "__main__":
   df = fetch_data("AAPL", "2024-01-01", "2025-01-01")
   returns = calculate_daily_returns(df)
+  print(returns)
   print("Volatility:", calculate_volatility(returns))
   print("VaR:", calculate_var(returns))
   plot_returns(returns)
